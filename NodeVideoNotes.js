f8a8e701737dcae0ffd530ec4e75f5662dc37e94
Austin Wright <awguitar86@gmail.com>
2018-02-09 10:55:01 -0700

    added notes on yarn

--- NodeVideoNotes.js
+++ NodeVideoNotes.js
@@ -17,3 +17,18 @@
 // a file that racks basic information about a project, including dependencies and versions. Created using npm init in the command line.
 
 
+/* YARN */
+// Yarn is made by Facebook
+// Includes NPM and Bower, both are package repos to get libraries
+
+/* yarn install */
+// yarn will look at the package.json and install all the dependencies.
+
+/* yarn add [package] */
+// yarn will install the package and add the dependency to the package.json. This is the same as npm install --save.
+
+/* npm install -g yarn */
+// install yarn globally, to be used everywhere.
+
+
+
