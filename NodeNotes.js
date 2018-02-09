43223ebc0f479d90ddde10656facfeb3fb92b7ed
Austin Wright <awguitar86@gmail.com>
2018-02-09 10:49:09 -0700

    commiting all files and folders in this folder to repo

---
+++ NodeNotes.js
@@ -0,0 +1,25 @@
+//NODE NOTES
+
+/*
+API = Application Programer Interface
+SPA = Single Page Website: Where a website does everything on one page instead of sending back and forth with a server.
+RESOURCE:
+
+
+HTTP = Hypertext Transfer Protocol: Using an agreed apon rule that communicates between a client and a server.
+
+PROTOCOL: A set of rules we agree on to do something.
+
+options
+GET: a way of getting some data
+
+POST: create. when you want to create a new resource. It does not exist yet.
+
+PUT/PATCH: update. It does exist and I;m doing some changes to it.
+
+DELETE: to destroy it.
+
+ENDPOINT:
+
+
+*/
---
+++ NodeVideoNotes.js
@@ -0,0 +1,19 @@
+/* NODE */
+
+/* npm install [package]*/
+// install the node package pf the name into the current folder
+
+/* npm install --save [package] */
+// installs the node package and adds the packege to the package.json dependencies
+
+/* npm install -g [package] */
+// installs the node package of the name into the computer's root folder, available across the device
+
+/* npm install */
+// looks in the package.json for everything saved under the dependencies, then installs those packages
+
+
+/* Package.json */
+// a file that racks basic information about a project, including dependencies and versions. Created using npm init in the command line.
+
+
