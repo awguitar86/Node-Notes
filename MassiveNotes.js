7f889bb17badf3796f6dde8f29c9fce9613a8ec5
Austin Wright <awguitar86@gmail.com>
2018-02-16 15:31:18 -0700

    wrote new file for massive notes

---
+++ MassiveNotes.js
@@ -0,0 +1,9 @@
+/*  MASSIVE  */
+
+//To install PostgreSQL
+    //Mac: http://postgresapp.com/
+
+// psql - connect to postgres in the terminal
+// CREATE DATABASE [database]; - create database
+// \c [database] - connect to database
+// \q - disconnect from postgres
