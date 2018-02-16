ffd0dbd31eb35b924fdce39d8fee30b169f32db0
Austin Wright <awguitar86@gmail.com>
2018-02-16 15:54:24 -0700

    wrote notes on query parameters

--- MassiveNotes.js
+++ MassiveNotes.js
@@ -7,3 +7,13 @@
 // CREATE DATABASE [database]; - create database
 // \c [database] - connect to database
 // \q - disconnect from postgres
+
+/* QUEARY PARAMETERS */
+//Takes everything after the question mark on a url and places it on req.query.
+//req.params takes the data that is in the url before the question mark.
+
+//For handling query parameters, we use the same endpoints as if there were no queries
+app.get('/api/species', function(req, res, next) {
+    req.query.group
+});
+//Express will put all the queries from the client request onto a query object on the req object.
